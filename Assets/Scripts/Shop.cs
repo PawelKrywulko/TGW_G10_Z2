2c796467b0eff772b39c9520c6c60a18dd5a6807
pkrywulko <pkrywulko@gmail.com>
2020-03-09 14:11:02 +0100

    added some improvements in shop

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -16,6 +16,9 @@ public class Shop : MonoBehaviour
     [SerializeField] private GameObject shopItemPrefab;
 
     private List<GameObject> shopItemsReferences = new List<GameObject>();
+    private Color defaultBackground = new Color(0.0f, 0.0f, 0.0f, 100.0f / 255f);
+    private Color lastlychosenColor = new Color(27.0f / 255f, 27.0f / 255f, 27.0f / 255f, 255.0f / 255f);
+    private Animator buttonAnimator;
 
     void Start()
     {
@@ -42,6 +45,13 @@ public class Shop : MonoBehaviour
                 Destroy(coinImageTransform.gameObject);
                 itemObject.transform.GetChild(2).GetChild(0).GetChild(1).GetComponent<Text>().text = "SOLD";
             }
+
+            var currentPlayerColorHexStr = $"#{ColorUtility.ToHtmlStringRGBA(itemObject.transform.GetChild(1).GetComponent<Image>().color)}";
+            if (currentPlayerColorHexStr == PlayerPrefs.GetString("CurrentPlayerColor", "#000000"))
+            {
+                //change background ShopItem if currently used
+                itemObject.GetComponent<Image>().color = lastlychosenColor;
+            }
             
             shopItemsReferences.Add(itemObject);
         });
@@ -49,17 +59,18 @@ public class Shop : MonoBehaviour
 
     private void OnButtonClick(ShopItem item)
     {
-        if(GameManager.allCoins >= item.price && PlayerPrefs.GetInt(item.itemName) == 0)
+        var itemReference = shopItemsReferences.First(itemRef => itemRef.transform.GetChild(0).GetComponent<Text>().text == item.itemName);
+        var itemColor = itemReference.transform.GetChild(1).GetComponent<Image>().color;
+        var colorHexStr = $"#{ColorUtility.ToHtmlStringRGBA(itemColor)}";
+        var buttonAnimator = itemReference.transform.GetChild(2).GetComponent<Animator>();
+
+        if (GameManager.allCoins >= item.price && PlayerPrefs.GetInt(item.itemName) == 0)
         {
-            var itemReference = shopItemsReferences.First(itemRef => itemRef.transform.GetChild(0).GetComponent<Text>().text == item.itemName);
             var coinImageTransform = itemReference.transform.GetChild(2).GetChild(0).GetChild(0).transform;
             Destroy(coinImageTransform.gameObject);
             itemReference.transform.GetChild(2).GetChild(0).GetChild(1).GetComponent<Text>().text = "SOLD";
-
-            var itemColor = itemReference.transform.GetChild(1).GetComponent<Image>().color;
-            var colorHashStr = $"#{ColorUtility.ToHtmlStringRGBA(itemColor)}";
             
-            PlayerPrefs.SetString("CurrentPlayerColor", colorHashStr);
+            PlayerPrefs.SetString("CurrentPlayerColor", colorHexStr);
             PlayerPrefs.SetInt(item.itemName, 1); //itemName == 1 => item SOLD
 
             GameEvents.Instance.HandleItemInShopBought(new ItemInShopBought
@@ -71,11 +82,24 @@ public class Shop : MonoBehaviour
 
         if(PlayerPrefs.GetInt(item.itemName) == 1)
         {
+            PlayerPrefs.SetString("CurrentPlayerColor", colorHexStr);
+
             GameEvents.Instance.HandleItemInShopBought(new ItemInShopBought
             {
                 ItemColor = item.skinColor,
                 ItemPrice = 0
             });
+        }
+
+        if(GameManager.allCoins < item.price && PlayerPrefs.GetInt(item.itemName) == 0)
+        {
+            buttonAnimator.SetTrigger("NotEnoughMoney");
+        } 
+        else
+        {
+            //change backgrounds
+            shopItemsReferences.ForEach(si => si.GetComponent<Image>().color = defaultBackground);
+            itemReference.GetComponent<Image>().color = lastlychosenColor;
         }
     }
 }
